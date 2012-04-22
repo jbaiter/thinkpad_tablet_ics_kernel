58e4f1aefb7ec1d31a54768564930a02dd0a447b
Johannes Baiter <johannes.baiter@gmail.com>
2012-04-22 21:22:09 +0200

    Cleaned up build files (=make mrproper) Added tegra_cm9_defconfig to support CM9 integrated kernel build

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 8 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#8 SMP PREEMPT Fri Mar 16 20:25:14 CET 2012"
-#define LINUX_COMPILE_TIME "20:25:14"
-#define LINUX_COMPILE_BY "koshu"
-#define LINUX_COMPILE_HOST "koshu-desktop"
-#define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
